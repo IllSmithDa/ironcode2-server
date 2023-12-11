ae2d934c5bbab2b98d2c9d307abed00aeb40d3c0
Samuel <sam404kim@gmail.com>
2023-12-11 13:13:03 -0800

    added procfile

--- allowedOrigins.js
+++ allowedOrigins.js
@@ -2,6 +2,7 @@ const allowedOrigins = [
   'http://localhost:3000',
   'http://localhost:19006',
   'http://192.168.0.13:19000',
+  'https://657762ba4938b200088ef7a4--willowy-otter-2f1119.netlify.app',
 ];
 
 module.exports = allowedOrigins;
