2a21f02dc62b443ea5384fe3809f2ab046e4ac40
Samuel <sam404kim@gmail.com>
2023-12-11 23:45:45 -0800

    port check

--- index.js
+++ index.js
@@ -60,7 +60,7 @@ app.all('*', (req, res, next) => {
 
 
 const port = process.env.PORT;
-app.listen(port | 5000, () => {
+app.listen(port || 5000, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
 
