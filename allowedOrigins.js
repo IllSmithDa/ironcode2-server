b340072ac942e9d3301b6b758cc736b34fe02229
Samuel <sam404kim@gmail.com>
2023-12-11 21:17:22 -0800

    added new origin

--- allowedOrigins.js
+++ allowedOrigins.js
@@ -3,7 +3,10 @@ const allowedOrigins = [
   'http://localhost:19006',
   'http://192.168.0.13:19000',
   'https://657762ba4938b200088ef7a4--willowy-otter-2f1119.netlify.app',
+  '657762ba4938b200088ef7a4--willowy-otter-2f1119.netlify.app',
   'https://ironcodeman.com',
+
+  'https://www.ironcodeman.com',
 ];
 
 module.exports = allowedOrigins;
